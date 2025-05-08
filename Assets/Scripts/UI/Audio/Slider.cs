5f5764135371b894bd1fe024078a8927450e892c
hotdogwithdog <hotdogwithdog@gmail.com>
2025-05-08 19:19:18 +0200

    Added the audio manager to the options not actually using the SFX group of the mixer but all works fine

---
+++ Assets/Scripts/UI/Audio/Slider.cs
@@ -0,0 +1,33 @@
+﻿using AudioSystem;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UI.Audio
+{
+    public class Slider : MonoBehaviour, IDragHandler
+    {
+        public delegate void OnSliderEvent(AudioChannel channel, float value);
+        public OnSliderEvent onSliderEvent;
+
+        private UnityEngine.UI.Slider _slider;
+        public float SldValue { get { return _slider.value; } set { _slider.value = value; } }
+
+        [SerializeField] private AudioChannel _channel;
+
+        public void Awake()
+        {
+            _slider = gameObject.GetComponent<UnityEngine.UI.Slider>();
+            _slider.value = AudioManager.Instance.GetVolumeByChannel( _channel );
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            onSliderEvent?.Invoke(_channel, _slider.value);
+        }
+
+        public AudioChannel GetChannel()
+        {
+            return _channel;
+        }
+    }
+}
